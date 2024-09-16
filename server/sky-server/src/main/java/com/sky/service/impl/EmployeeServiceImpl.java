08f68e5a7ec4b239c66562c2b16eed24fc4f87ac
Ding07 <14344095+ding07@user.noreply.gitee.com>
2024-09-16 21:18:49 +0800

    增加启用禁用员工账号功能

--- server/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ server/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -102,4 +102,20 @@ public class EmployeeServiceImpl implements EmployeeService {
         List<Employee> records = page.getResult();
         return new PageResult(total,records);
     }
+
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     */
+    public void startOrStop(Integer status, Long id){
+        // Employee employee = new Employee();
+        // employee.setId(id);
+        // employee.setStatus(status);
+        Employee employee = Employee.builder()
+                .status(status)
+                .id(id)
+                .build();
+        employeeMapper.update(employee);
+    }
 }
