e4fa06f009f2878531229f59de839c404ce3705e
Ding-Lifeng <2576199644@qq.com>
2024-09-19 21:19:30 +0800

    增加修改菜品和信息回显功能

--- server/sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ server/sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -3,6 +3,7 @@ package com.sky.mapper;
 import com.sky.entity.DishFlavor;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -17,8 +18,22 @@ public interface DishFlavorMapper {
 
     /**
      * 根据菜品Id删除口味
-     * @param id
+     * @param dishId
      */
     @Delete("delete from dish_flavor where dish_id = #{dishId}")
     void deleteByDishId(long dishId);
+
+    /**
+     * 根据菜品Id集合批量删除关联的口味数据
+     * @param dishIds
+     */
+    void deleteByDishIds(List<Long> dishIds);
+
+    /**
+     * 根据菜品id查询对应的口味数据
+     * @param dishId
+     * @return
+     */
+    @Select("select * from dish_flavor where dish_id = #{dishId}")
+    List<DishFlavor> getByDishId(Long dishId);
 }
