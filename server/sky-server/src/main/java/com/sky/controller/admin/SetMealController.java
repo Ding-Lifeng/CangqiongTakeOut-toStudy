8199bec3d2235406c4e64741fb23b464a752f47e
Ding-Lifeng <2576199644@qq.com>
2024-09-20 22:26:34 +0800

    删除套餐

--- server/sky-server/src/main/java/com/sky/controller/admin/SetMealController.java
+++ server/sky-server/src/main/java/com/sky/controller/admin/SetMealController.java
@@ -11,6 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 /**
  * 套餐管理
  */
@@ -44,5 +46,17 @@ public class SetMealController {
     public Result<PageResult> page(SetmealPageQueryDTO setmealPageQueryDTO) {
         PageResult pageResult = setmealService.pageQuery(setmealPageQueryDTO);
         return Result.success(pageResult);
+    }
+
+    /**
+     * 批量删除套餐
+     * @param ids
+     * @return
+     */
+    @DeleteMapping
+    @ApiOperation("批量删除套餐")
+    public Result delete(@RequestParam List<Long> ids){
+        setmealService.deleteBatch(ids);
+        return Result.success();
     }
 }
--- server/sky-server/src/main/java/com/sky/service/SetmealService.java
+++ server/sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -4,6 +4,8 @@ import com.sky.dto.SetmealDTO;
 import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.result.PageResult;
 
+import java.util.List;
+
 public interface SetmealService {
     /**
      * 新增套餐 保存套餐和菜品的关联关系
@@ -17,4 +19,10 @@ public interface SetmealService {
      * @return
      */
     PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
+
+    /**
+     * 批量删除套餐
+     * @param ids
+     */
+    void deleteBatch(List<Long> ids);
 }
